bbecea96e2e562bfd6ad956a8fc400b5c7eaec67
Cena2009 <1592024316@qq.com>
2020-11-17 17:31:38 +0800

    添加了启动类的注解支持远程调用了

--- gt-freemarker/src/main/java/com/go/FreemarkerApplication.java
+++ gt-freemarker/src/main/java/com/go/FreemarkerApplication.java
@@ -3,10 +3,11 @@ package com.go;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
+@EnableFeignClients
 public class FreemarkerApplication {
 
     public static void main(String[] args) {
